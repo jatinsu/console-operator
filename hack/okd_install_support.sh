0e944edc9252409baab1e57e7d6232cf01444031
Jatin Suri <jsuri@redhat.com>
2026-01-07 18:16:23 -0500

    Found a way to disable quickstarts properly

--- hack/okd_install_support.sh
+++ hack/okd_install_support.sh
@@ -2,9 +2,19 @@
 
 set -euo pipefail
 
+# Function to set all include.release.openshift.io annotations to false
+set_annotations_to_false() {
+  local file="$1"
+  sed -i "/include\.release\.openshift\.io/s/true/false/g" "$file"
+}
+
 source /etc/os-release
 if [ "$ID" == "centos" ]; then
-    cp /manifests/okd_quickstarts/toolhive-quickstart.yaml /manifests/toolhive-quickstart.yaml
-    cp /manifests/okd_quickstarts/do-not-move.yaml /manifests/do-not-move.yaml
-    rm -f /manifests/install-multicluster-engine.yaml
+  cp /manifests/okd_quickstarts/toolhive-quickstart.yaml /manifests/toolhive-quickstart.yaml
+  cp /manifests/okd_quickstarts/do-not-move.yaml /manifests/do-not-move.yaml
+
+  # Set all include.release.openshift.io annotations to false
+  set_annotations_to_false /manifests/install-multicluster-engine.yaml
+  set_annotations_to_false /manifests/do-not-move.yaml
 fi
+
